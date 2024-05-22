c7e37c1eecda5638e2b4b94bac677af4ebed9204
theblvckdev <nathancodes05@gmail.com>
2024-05-22 23:02:16 +0100

    removed logout button

--- src/pages/home/home_page.jsx
+++ src/pages/home/home_page.jsx
@@ -43,14 +43,6 @@ export default function Homepage() {
 
   return (
     <>
-      <div className="fixed bottom-0 right-0 m-5 w-fit group">
-        <button className="h-[50px] w-[50px] ring-1 ring-gray-500 bg-gray-200 text-gray-900 text-2xl rounded-full flex items-center justify-center shadow-md">
-          <IoExitOutline />
-        </button>
-        <div className="absolute top-[50%] -left-[170%] opacity-0 duration-300 ease-in group-hover:opacity-100 -translate-y-[50%] bg-black bg-opacity-90 text-white text-[12px] text-center p-1 font-poppins w-[80px] rounded-md h-fit">
-          Log out
-        </div>
-      </div>
       <Navbar />
       <main className="bg-gray-100 min-h-screen h-full md:py-24 py-32">
         <div className="max-w-6xl mx-auto w-full lg:px-0 md:px-5 px-3">
