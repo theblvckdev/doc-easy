dbba61ef54200af7fbf4297a4f07cc7abeff3a69
theblvckdev <nathancodes05@gmail.com>
2024-06-06 19:48:04 +0100

    worked on downloading files from api.

--- src/pages/home/home_page.jsx
+++ src/pages/home/home_page.jsx
@@ -7,7 +7,6 @@ import axios from "axios";
 export default function Homepage() {
   const userData = JSON.parse(localStorage.getItem("user")) || null;
   const [loading, setLoading] = useState(false);
-
   const [documents, setDocuments] = useState([]);
 
   useEffect(() => {
@@ -25,7 +24,6 @@ export default function Homepage() {
           }
         );
 
-        console.log(response);
         setLoading(false);
         setDocuments(response.data.results);
       } catch (err) {
@@ -37,30 +35,25 @@ export default function Homepage() {
     getDocs();
   }, []);
 
-  const downloadFile = async () => {
+  const downloadFile = async (url, filename) => {
     try {
-      const response = axios.get(
-        "http://supremepraiz.pythonanywhere.com/niger/document/1/surveyor/",
-        {
-          responseType: "blob",
-        }
-      );
-
-      const blob = new Blob([response.data.results], {
-        type: response.data.results.file,
+      const response = await axios.get(url, {
+        responseType: "blob", // Important for downloading files
       });
-      const url = window.URL.createObjectURL(blob);
+
+      const blob = new Blob([response.data], { type: response.data.type });
+      const blobUrl = window.URL.createObjectURL(blob); // Creating a Blob URL
 
       const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", `${response.data.results.file_name}.zip`);
+      link.href = blobUrl;
+      link.setAttribute("download", filename); // The filename and extension
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
 
-      window.URL.revokeObjectURL(URL);
-    } catch (err) {
-      console.error("Error downloading the file", err);
+      window.URL.revokeObjectURL(blobUrl); // Clean up the URL object
+    } catch (error) {
+      console.error("Error downloading the file", error);
     }
   };
 
@@ -73,9 +66,6 @@ export default function Homepage() {
             Welcome back{" "}
             <span className="capitalize">{userData?.username}</span>
           </h1>
-          {/* <h4 className="text-gray-400 md:text-xl text-base font-light font-poppins mt-1">
-            
-          </h4> */}
 
           <div className="md:mt-8 mt-5">
             {loading ? (
@@ -92,43 +82,36 @@ export default function Homepage() {
                   const { file, file_name, location, surveyor } = data;
 
                   return (
-                    <>
-                      <div
-                        key={index}
-                        className="p-2 px-3 outline-none no-underline bg-white overflow-hidden shadow ring-1 ring-gray-200 rounded-[10px] text-gray-900"
-                      >
-                        <div className="flex items-center gap-3">
-                          <IoFolderOutline className="text-6xl" />
+                    <div
+                      key={index}
+                      className="p-2 px-3 outline-none no-underline bg-white overflow-hidden shadow ring-1 ring-gray-200 rounded-[10px] text-gray-900"
+                    >
+                      <div className="flex items-center gap-3">
+                        <IoFolderOutline className="text-6xl" />
 
-                          <div>
-                            <div className="text-sm font-bold font-poppins truncate max-w-[200px]">
-                              {file_name}
+                        <div>
+                          <div className="text-sm font-bold font-poppins truncate max-w-[200px]">
+                            {file_name}
+                          </div>
+                          <div className="flex flex-col text-[12px]">
+                            <div className="text-gray-400 font-poppins w-full">
+                              Surveyor: {surveyor}
                             </div>
-                            <div className="flex flex-col text-[12px]">
-                              <div className="text-gray-400 font-poppins w-full">
-                                Surveyor: {surveyor}
-                              </div>
 
-                              <div className="text-gray-400 font-poppins w-full">
-                                Location: {location}
-                              </div>
+                            <div className="text-gray-400 font-poppins w-full">
+                              Location: {location}
                             </div>
                           </div>
-                          <a
-                            href={file}
-                            download={`${file_name}.zip`}
-                            className="outline-none"
-                          >
-                            <button
-                              onClick={downloadFile}
-                              className="border-l-2 px-2 ml-auto outline-none"
-                            >
-                              <IoDownloadOutline className="text-4xl text-green-500" />
-                            </button>
-                          </a>
                         </div>
+
+                        <button
+                          onClick={() => downloadFile(file, `${file_name}.exe`)}
+                          className="border-l-2 px-2 ml-auto outline-none"
+                        >
+                          <IoDownloadOutline className="text-4xl text-green-500" />
+                        </button>
                       </div>
-                    </>
+                    </div>
                   );
                 })}
               </div>
